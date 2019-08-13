f5bd067c24716031ade1d95ccdcc3ce90b978b7d
Amy Fong <amyhfong@gmail.com>
2019-08-13 19:08:27 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -33,6 +33,12 @@ def find_a(array)
   }
 end
 
+# using find_all method
+# def find_a(array)
+#   array.find_all do |word|
+#     word[0] == "a"
+#   end
+
 def sum_array(array)
     array.inject {|sum, n|
     sum + n
