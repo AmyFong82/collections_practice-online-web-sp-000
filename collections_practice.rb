e6e7577d60cd2045b767418a7c717be74cfa4f47
Amy Fong <amyhfong@gmail.com>
2019-08-13 19:05:53 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -47,3 +47,13 @@ end
 
 array = [1, 2, 3]
 array [0], array[1], array[2] = array[0], array[2], array[1]
+
+def kesha_maker(array)
+  array.each do |item|
+    item[2] = "$"
+  end
+end
+
+array = ["aaaa", 'bbbb']
+
+kesha_maker(array)
